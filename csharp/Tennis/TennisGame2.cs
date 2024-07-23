a614338479c4e0d6fe4435f73b1579c1a8334c22
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-07-23 23:47:05 +0300

    Add partial refactoring of the code.

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -2,102 +2,102 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int _p1point;
+        private int _p2point;
 
-        private string p1res = "";
-        private string p2res = "";
-        private string player1Name;
-        private string player2Name;
+        private string _p1res = "";
+        private string _p2res = "";
+        private string _player1Name;
+        private string _player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            p1point = 0;
-            this.player2Name = player2Name;
+            this._player1Name = player1Name;
+            this._p1point = 0;
+            this._player2Name = player2Name;
         }
 
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
+            if (_p1point == _p2point && _p1point < 3)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                score += $"{Score(this._p1point)}-All";
             }
-            if (p1point == p2point && p1point > 2)
+            if (_p1point == _p2point && _p1point > 2)
+            {
                 score = "Deuce";
-
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
             }
 
-            if (p1point > p2point && p1point < 4)
+            if (_p1point > 0 && _p2point == 0)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
+                // this._p1res = Score(this._p1point);
+                if (_p1point == 1)
+                {
+                    _p1res = "Fifteen";
+                }
+                else if (_p1point == 2)
+                {
+                    _p1res = "Thirty";
+                }
+                else if (_p1point == 3)
+                {
+                    _p1res = "Forty";
+                }
+
+
+                _p2res = "Love";
+                score = $"{this._p1res}-{this._p2res}";
             }
-            if (p2point > p1point && p2point < 4)
+            if (_p2point > 0 && _p1point == 0)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
+                if (_p2point == 1)
+                {
+                    _p2res = "Fifteen";
+                }
+                if (_p2point == 2)
+                {
+                    _p2res = "Thirty";
+                }
+                if (_p2point == 3)
+                {
+                    _p2res = "Forty";
+                }
+
+                _p1res = "Love";
+                score = $"{this._p1res}-{this._p2res}";
             }
 
-            if (p1point > p2point && p2point >= 3)
+            if (_p1point > _p2point && _p1point < 4)
+            {
+                this._p1res = Score(_p1point);
+                this._p2res = Score(_p2point);
+
+                score = $"{this._p1res}-{this._p2res}";
+            }
+            if (_p2point > _p1point && _p2point < 4)
+            {
+                _p2res = Score(this._p2point);
+                _p1res = Score(this._p1point);
+
+                score = $"{this._p1res}-{this._p2res}";
+            }
+
+            if (_p1point > _p2point && _p2point >= 3)
             {
                 score = "Advantage player1";
             }
 
-            if (p2point > p1point && p1point >= 3)
+            if (_p2point > _p1point && _p1point >= 3)
             {
                 score = "Advantage player2";
             }
 
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            if (_p1point >= 4 && _p2point >= 0 && (_p1point - _p2point) >= 2)
             {
                 score = "Win for player1";
             }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+            if (_p2point >= 4 && _p1point >= 0 && (_p2point - _p1point) >= 2)
             {
                 score = "Win for player2";
             }
@@ -106,38 +106,36 @@ namespace Tennis
 
         public void SetP1Score(int number)
         {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
+            PlayerScore(ref this._p2point, number);
         }
 
         public void SetP2Score(int number)
         {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
+            PlayerScore(ref this._p2point, number);
         }
 
         public void WonPoint(string player)
         {
-            if (player == "player1")
-                P1Score();
+            if (player == this._player1Name)
+            {
+                PlayerScore(ref this._p1point, 1);
+            }
             else
-                P2Score();
+            {
+                PlayerScore(ref this._p2point, 1);
+            }
         }
-
+        private static string Score(int playerScore)
+              => playerScore switch
+              {
+                  0 => "Love",
+                  1 => "Fifteen",
+                  2 => "Thirty",
+                  3 => "Forty"
+              };
+        private static void PlayerScore(ref int playerScore, int number)
+        {
+            playerScore += number;
+        }
     }
 }
-
