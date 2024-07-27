91eae72823eab836f64bf16c42b3565d19b7c794
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-07-27 23:04:31 +0300

    Add refactored TernisGame3.

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -1,43 +1,66 @@
 namespace Tennis
 {
-    public class TennisGame3 : ITennisGame
+    public class TennisGame3(string player1Name, string player2Name)
+        : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
-
-        public TennisGame3(string player1Name, string player2Name)
-        {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
-        }
+        private int player1;
+        private int player2;
+        private readonly string _player1Name = player1Name;
+        private readonly string _player2Name = player2Name;
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            if (this.IsNotDraw())
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                var score = Score(this.player1);
+
+                return (this.player1 == this.player2)
+                    ? $"{score}-All"
+                    : $"{score}-{Score(player2)}";
             }
             else
             {
-                if (p1 == p2)
+                if (this.player1 == this.player2)
+                {
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                }
+
+                string s = this.player1 > this.player2
+                     ? this._player1Name
+                     : this._player2Name;
+
+                return IsAdvantage()
+                    ? $"Advantage {s}"
+                    : $"Win for {s}";
             }
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+            {
+                this.player1 += 1;
+            }
             else
-                this.p2 += 1;
+            {
+                this.player2 += 1;
+            }
         }
+        private static string Score(int playerScore)
+                => playerScore switch
+                {
+                    0 => "Love",
+                    1 => "Fifteen",
+                    2 => "Thirty",
+                    3 => "Forty",
+                    _ => ""
+                };
 
+        private bool IsAdvantage()
+            => (this.player1 - this.player2) * (this.player1 - this.player2) == 1;
+
+        private bool IsNotDraw()
+            => (this.player1 < 4 && this.player2 < 4)
+            && (this.player1 + this.player2 < 6);
     }
 }
